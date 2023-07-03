f3d2cfe19c01c46659027d0972734c53196f9d1d
rishabh.garg <rishabh.garg@electrifai.com>
2023-07-04 00:43:57 +0530

    AWS Region variablised, FLUX CLI added, Updated Readme

--- single-liner/new.sh
+++ single-liner/new.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 tfenv install ${TF_VERSION} 
 tfenv use ${TF_VERSION}  
-aws configure set region us-east-1
+aws configure set region ${AWS_REGION}
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws eks update-kubeconfig --region us-east-1 --name ${NAME_OF_CLUSTER}
+
+#Extra Utilities. 
+#If value of the corresponding env variable is not set, it will not be installed.
+############ FLUX CLI ##############
+if [[ -n "$FLUX_VERSION" ]]; then
+    curl -s https://fluxcd.io/install.sh | bash
+fi
+
 /bin/bash
